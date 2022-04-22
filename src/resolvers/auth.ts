79b4d38f373b4969ef7ec4931d071804e1576be9
Ankit Sharma <sankit945@gmail.com>
2022-04-22 14:03:28 +0530

    added bcrypt

--- src/resolvers/auth.ts
+++ src/resolvers/auth.ts
@@ -24,8 +24,8 @@ export class AuthResolver {
         @Arg('username', () => String) username: string,
         @Arg('password', () => String) password: string
     ): Promise<LoginResponse> {
-        const user = await UserService.login(username, password);
-        if (!user) throw new Error('Invalid username or password');
+        const user = await UserService.verifyUserNameAndPassword(username, password);
+        if (!user) throw new Error('Invalid password');
         return {
             accessToken: jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' })
         }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,20 @@
 import { IUser, User } from "../models/user.model";
+import bcrypt from 'bcrypt';
 
 export class UserService {
     static async getUserById(id: string): Promise<IUser | null> {
         return await User.findById(id);
     }
-    static createUser(username: string, password: string): Promise<IUser> {
-        return User.create({ username, password });
+    static async createUser(username: string, password: string): Promise<IUser> {
+        const hash = await bcrypt.hash(password, 10);
+        return User.create({ username, password: hash });
     }
-    static async login(username: string, password: string): Promise<IUser | null> {
-        return await User.findOne({ username, password });
+    static async verifyUserNameAndPassword(username: string, password: string): Promise<IUser | null> {
+        const user= await User.findOne({ username });
+        if (!user) throw new Error('User Not Found')
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) return null;
+        return user;
     }
 }
 
